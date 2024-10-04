e3562790f94033f1fb36147a7a1fcf3dabc15a54
PranaviJadhav5774 <jadhavpranavi5774@gmail.com>
2024-10-04 09:06:32 +0530

    Add files via upload

---
+++ p2.py
@@ -0,0 +1,16 @@
+a=1
+b=2
+sum=a+b
+mul=a*b
+sub=a-b
+div=a/b
+exp=a**b
+modulus=a%b
+
+
+print("sum=",sum)
+print("sub=",sub)
+print("mul=",mul)
+print("div=",div)
+print("exp=",exp)
+print("modulus=",modulus)
